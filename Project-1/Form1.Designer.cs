f2b6a4c031779b928005e525aefc2a7245802a36
Brandon Gerber <90160923+brandongerber@users.noreply.github.com>
2024-10-07 17:11:50 -0400

    fininished needs some work tho

--- Project-1/Form1.Designer.cs
+++ Project-1/Form1.Designer.cs
@@ -1,16 +1,11 @@
-﻿namespace Project_1
+﻿using System;
+
+namespace Project_1
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +15,6 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -33,7 +22,6 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -44,65 +32,64 @@
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(118, 33);
+            this.textBox1.Location = new System.Drawing.Point(95, 45);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(172, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(118, 179);
+            this.textBox2.Location = new System.Drawing.Point(95, 191);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(212, 20);
             this.textBox2.TabIndex = 1;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(118, 237);
+            this.textBox3.Location = new System.Drawing.Point(95, 249);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(172, 20);
             this.textBox3.TabIndex = 2;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(118, 75);
+            this.textBox5.Location = new System.Drawing.Point(95, 87);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(172, 20);
             this.textBox5.TabIndex = 4;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(118, 125);
+            this.textBox6.Location = new System.Drawing.Point(95, 137);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(240, 20);
             this.textBox6.TabIndex = 5;
             // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(799, 172);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 7;
-            // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Yellow;
-            this.button3.Location = new System.Drawing.Point(521, 566);
+            this.button3.Location = new System.Drawing.Point(594, 582);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(88, 32);
             this.button3.TabIndex = 10;
             this.button3.Text = "Reset";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 33);
+            this.label1.Location = new System.Drawing.Point(17, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 11;
@@ -111,7 +98,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(43, 81);
+            this.label2.Location = new System.Drawing.Point(20, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 12;
@@ -120,7 +107,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(43, 125);
+            this.label3.Location = new System.Drawing.Point(20, 137);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(45, 13);
             this.label3.TabIndex = 13;
@@ -129,7 +116,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(43, 179);
+            this.label4.Location = new System.Drawing.Point(20, 191);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(24, 13);
             this.label4.TabIndex = 14;
@@ -138,7 +125,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(43, 243);
+            this.label5.Location = new System.Drawing.Point(20, 255);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(50, 13);
             this.label5.TabIndex = 15;
@@ -147,33 +134,36 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Green;
-            this.button2.Location = new System.Drawing.Point(657, 566);
+            this.button2.Location = new System.Drawing.Point(730, 582);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 32);
             this.button2.TabIndex = 16;
             this.button2.Text = "Submit";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Maroon;
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(799, 566);
+            this.button1.Location = new System.Drawing.Point(872, 582);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 32);
             this.button1.TabIndex = 17;
             this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(46, 432);
+            this.checkBox1.Location = new System.Drawing.Point(20, 478);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(212, 17);
             this.checkBox1.TabIndex = 18;
-            this.checkBox1.Text = "All Information I have inputed is correct.";
+            this.checkBox1.Text = ".All Information I have inputed is correct";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // checkedListBox1
             // 
@@ -183,49 +173,112 @@
             "Afternoon Shift",
             "Evening Shift",
             "Weekend Shift"});
-            this.checkedListBox1.Location = new System.Drawing.Point(46, 277);
+            this.checkedListBox1.Location = new System.Drawing.Point(17, 382);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(244, 124);
+            this.checkedListBox1.Size = new System.Drawing.Size(130, 64);
             this.checkedListBox1.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "?Are you over 18 years old";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.checkedListBox2.Location = new System.Drawing.Point(20, 318);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(59, 34);
+            this.checkedListBox2.TabIndex = 21;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 366);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "?Preferred Shifts";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::Project_1.Properties.Resources.KFSCIS_vrt_COLOR;
+            this.pictureBox1.Location = new System.Drawing.Point(995, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(446, 181);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(486, 1);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(595, 37);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Student Assistant Position Application";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gray;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(1442, 626);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Project 1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Job Application Form";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
-        #endregion
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Enable or disable the Submit button based on checkbox state
+            button2.Enabled = checkBox1.Checked;
+        }
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -236,6 +289,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
     }
 }
-
--- Project-1/Form1.cs
+++ Project-1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Project_1
@@ -19,25 +12,112 @@ namespace Project_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // Any initialization code can go here
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
+            // Handle text changed event if needed
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
+            // Handle label click event if needed
+        }
 
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Handle checked list box selection changed event if needed
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Clear all text boxes
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+
+            // Reset checked items in checkedListBox1 (Preferred Shift)
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+
+            // Reset checked items in checkedListBox2 (Age Verification)
+            for (int i = 0; i < checkedListBox2.Items.Count; i++)
+            {
+                checkedListBox2.SetItemChecked(i, false);
+            }
+
+            // Uncheck the confirmation checkbox
+            checkBox1.Checked = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Validate input
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || // First Name
+                string.IsNullOrWhiteSpace(textBox2.Text) || // Last Name
+                string.IsNullOrWhiteSpace(textBox3.Text) || // Address
+                string.IsNullOrWhiteSpace(textBox5.Text) || // City
+                string.IsNullOrWhiteSpace(textBox6.Text))   // Zip Code
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return; // Exit the method if any fields are empty
+            }
+
+            // Check if at least one preferred shift is selected
+            bool shiftSelected = false;
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    shiftSelected = true;
+                    break;
+                }
+            }
+
+            if (!shiftSelected)
+            {
+                MessageBox.Show("Please select at least one preferred shift.");
+                return; // Exit if no shifts are selected
+            }
+
+            // Check if over 18 is selected
+            bool over18Selected = checkedListBox2.GetItemChecked(0); // Yes
+            if (!over18Selected)
+            {
+                MessageBox.Show("You must confirm that you are over 18 years old.");
+                return; // Exit if not confirmed
+            }
+
+            // All validations passed, you can proceed with the submission logic
+            MessageBox.Show("All information submitted successfully.");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Exit the application
+            Application.Exit();
+        }
+
+        private void checkedListBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
         {
 
         }
